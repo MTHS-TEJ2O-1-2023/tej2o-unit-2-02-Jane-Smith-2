8d2c527167ade31ab4afb3d62395aebefaf80db9
Jane-Smith-2 <64223840+Jane-Smith-2@users.noreply.github.com>
2023-08-24 14:24:55 -0400

    typescript version

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
+ * Created by: Jane Smith
  * Created on: Sep 2020
  * This program ...
 */
 
-basic.showString('Hello, World!')
+basic.showString('Hello, MTHS!')
